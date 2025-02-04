7d92187016d3acdc5d0b3579a0827b309990785a
Rizqiansyah Ramadhan <rizzthenotable@gmail.com>
2025-02-04 18:48:04 +0700

    fix deploy 2

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ import { test } from "@/test/test";
 const PORT = process.env.port || 5000;
 const app = express();
 test();
+
+app.use("/", (req, res) => {
+  res.send("helo");
+});
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}/`);
+});
